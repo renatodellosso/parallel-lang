1520128372c64006e17d3f3ad414ce2b5c4474ca
Renato Dell'Osso <rddellosso4@gmail.com>
2026-03-23 12:26:07 -0400

    Make dummy compile function

--- src/compiler/compiler.cpp
+++ src/compiler/compiler.cpp
@@ -1,9 +1,17 @@
 #include "compiler.hpp"
 #include "tokenizer.hpp"
+#include "../logging.hpp"
 #include <fstream>
 
 ExitCode compile(const CliArgs &args)
 {
   std::ifstream stream(args.target);
-  // auto tokens = tokenize(std::move(stream));
+  Tokenizer *tokenizer = new Tokenizer(stream);
+
+  tokenizer->parse();
+  auto tokens = tokenizer->close();
+  delete tokenizer;
+
+  log("Compiler", "Compiled {:d} tokens", tokens.get()->size());
+  return ExitCode::Ok;
 }
