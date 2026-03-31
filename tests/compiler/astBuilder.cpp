206607766fa7591d266b6e7768c00c560a72b99d
Renato Dell'Osso <rddellosso4@gmail.com>
2026-03-30 21:45:07 -0400

    Test for building multiple lines

--- tests/compiler/astBuilder.cpp
+++ tests/compiler/astBuilder.cpp
@@ -67,3 +67,49 @@ TEST(AstBuilder, buildsBinaryExpressions)
   EXPECT_TOKEN_EQ(left->token, tokens[0]);
   EXPECT_TOKEN_EQ(right->token, tokens[2]);
 }
+
+TEST(AstBuilder, buildsMultipleLines)
+{
+  std::vector<Token> tokens = {
+      {TokenType::Literal,
+       TokenSubtype::Number,
+       "1",
+       1},
+      {TokenType::Plus,
+       TokenSubtype::None,
+       "+",
+       1},
+      {TokenType::Literal,
+       TokenSubtype::Number,
+       "2",
+       1},
+      {TokenType::Semicolon, TokenSubtype::None, ";", 1},
+      {TokenType::Literal,
+       TokenSubtype::Number,
+       "2",
+       2},
+      {TokenType::Minus,
+       TokenSubtype::None,
+       "-",
+       2},
+      {TokenType::Literal,
+       TokenSubtype::Number,
+       "3",
+       2},
+      {TokenType::Semicolon, TokenSubtype::None, ";", 2}};
+
+  AstBuilder builder(std::make_unique<std::vector<Token>>(tokens));
+
+  builder.build();
+
+  EXPECT_EQ(builder.getErrors().get()->size(), 0);
+
+  auto root = builder.getRoot();
+  ASSERT_EQ(root.get()->expressions.size(), 2); // Assert to avoid segfault if no expressions
+
+  auto expr = root.get()->expressions[0].get();
+  EXPECT_EQ(expr->type, InstructionType::Add);
+
+  expr = root.get()->expressions[1].get();
+  EXPECT_EQ(expr->type, InstructionType::Subtract);
+}
