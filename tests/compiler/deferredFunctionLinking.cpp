9e87f9e2ed4749ce292a4c259e346abbd5922e7f
Renato Dell'Osso <rddellosso4@gmail.com>
2026-05-12 23:00:45 -0400

    Fix DeferredFunctionLinking tests

--- tests/compiler/deferredFunctionLinking.cpp
+++ tests/compiler/deferredFunctionLinking.cpp
@@ -2,14 +2,30 @@
 #include <gtest/gtest.h>
 #include <memory>
 #include <unordered_set>
+#include <vector>
+
+std::shared_ptr<Function> makeFunction() {
+  Instruction first(0);
+  first.bytecodeArgs = {{ValueType::String, "returnType"},
+                        {ValueType::String, "name"}};
+
+  Instruction second(1);
+  second.bytecodeArgs = {{ValueType::Integer, 0}};
+
+  auto instructions = std::make_shared<std::vector<Instruction>>();
+  instructions->push_back(first);
+  instructions->push_back(second);
+
+  Subprogram program(instructions);
+
+  return std::make_shared<Function>(first, program);
+}
 
 TEST(DeferredFunctionLinking, producesConsistentHashes) {
   Instruction instr(0);
   Subprogram program;
 
-  auto func = std::shared_ptr<Function>();
-  // Don't use make_shared to avoid the constructor (we'd need a fully-formed
-  // set of instructions for that)
+  auto func = makeFunction();
   auto scope = std::make_shared<Scope<Resource>>();
 
   DeferredFunctionLinking defer = {func, scope};
@@ -25,9 +41,7 @@ TEST(DeferredFunctionLinking, preventsDuplicatesInUnorderedSets) {
   Instruction instr(0);
   Subprogram program;
 
-  auto func = std::shared_ptr<Function>();
-  // Don't use make_shared to avoid the constructor (we'd need a fully-formed
-  // set of instructions for that)
+  auto func = makeFunction();
   auto scope = std::make_shared<Scope<Resource>>();
 
   DeferredFunctionLinking defer1 = {func, scope};
@@ -39,4 +53,23 @@ TEST(DeferredFunctionLinking, preventsDuplicatesInUnorderedSets) {
   set.insert(defer2);
 
   EXPECT_EQ(set.size(), 1);
+}
+
+TEST(DeferredFunctionLinking, allowsDifferentInUnorderedSets) {
+  Instruction instr(0);
+  Subprogram program;
+
+  auto func1 = makeFunction();
+  auto func2 = makeFunction();
+  auto scope = std::make_shared<Scope<Resource>>();
+
+  DeferredFunctionLinking defer1 = {func1, scope};
+  DeferredFunctionLinking defer2 = {func2, scope};
+
+  std::unordered_set<DeferredFunctionLinking> set;
+
+  set.insert(defer1);
+  set.insert(defer2);
+
+  EXPECT_EQ(set.size(), 2);
 }
